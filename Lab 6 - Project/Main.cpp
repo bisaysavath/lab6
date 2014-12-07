77ed5bd6b3b0f7ed4a3a5ecd7b0e2c5074d072b8
Bounpaseuth Saysavath <bisaysavath@csu.fullerton.edu>
2014-12-07 15:53:05 -0800

    Able to extract data from S-to-D command and output as in a table format

--- Lab 6 - Project/Main.cpp
+++ Lab 6 - Project/Main.cpp
@@ -7,23 +7,27 @@
 #include<string>
 #include<cstdlib>
 #include<sstream>
+#include<queue>
 
 using namespace std;
 
 const string S_TO_D_COMMAND_VALUE = "40000810";
 const string D_TO_S_COMMAND_VALUE = "40000C18";
+//const string S_TO_D_COMMAND_RANGE[] =  {"40000818", "4000081C", "40000820", "40000824", "40000828", "4000082C", "40000830", "40000834",
+//										"40000838", "4000083C"};
 const int NUMBER_OF_WORDS_IN_A_LINE = 14;
 
 void outputToScreen( const string &address, const string &data, const int &size, const string &cycle );
-unsigned int convertHexaToUnsignedInt( const string &hexVariable );
+unsigned int convertHexStringToUnsignedInt( const string &hexVariable );
 int convertSizeTypeToInt( const string &sizeType );
+string convertHexIntToDecimalString( const int &hexValue );
 
 // Tool Function to pause the screen
 void pause( std::string prompt = "Press ENTER to continue..." );
 // Funtion to get boolean from user
 bool	getBool( std::string prompt );
 
-int main( void )
+int main( int argc, char *argv[] )
 {
 	ifstream inFile;
 	int numberOfWordEncounter = 0;
@@ -35,6 +39,14 @@ int main( void )
 		pause( "Press enter to exit." );
 		exit( 1 );
 	}
+
+	//int a = 0xabc;
+
+	//cout << convertHexStringToUnsignedInt("40000818") << endl;
+	//cout << convertHexIntToDecimalString( 0x40000818 ) << endl;
+
+	//if( convertHexIntToDecimalString( 0x40000818 ) == convertHexIntToDecimalString( convertHexStringToUnsignedInt( "40000818" ) ) )
+	//	cout << "Same" << endl;
 
 	string address;
 	string data;
@@ -61,35 +73,77 @@ int main( void )
 		outputToScreen( address, data, sizeInInt, cycle );
 
 		// Convert the hex in data variable to unsigned int
-		unsigned int wordCount = convertHexaToUnsignedInt( data );
+		unsigned int wordCount = convertHexStringToUnsignedInt( data );
 		cout << "Number of words: " << wordCount << endl;
 
 		// Output line number in a log file of the current address
 		cout << "Line number: " << (numberOfWordEncounter / NUMBER_OF_WORDS_IN_A_LINE) + 1 << endl;
 
-		//if (wordCount > 0)
-		//{
-		//	if (address == S_TO_D_COMMAND_VALUE)
-		//	{
-		//		// search from the address of 40000818 to 40000C14
-		//		int startSearchingAddress = 0x40000818;
-		//		int stopSearchingAddress = 0x40000C14;
-		//		string nextWordOnALine;
-		//		
-		//		do
-		//		{
-		//			do
-		//			{
-		//				inFile >> nextWordOnALine;
-		//			} while( nextWordOnALine != "40000818" );
+		queue<string> wordsData;
 
-		//			inFile >> data;
+		if (wordCount > 0)
+		{
+			if (address == S_TO_D_COMMAND_VALUE)
+			{
+				// search from the address of 40000818 to 40000C14
+				int nextSearchingAddress = 0x40000818;
+				int stopSearchingAddress = 0x40000C14;
+				string nextWordOnALine;
+				int loopCount = 0;
+				bool stopSearch = false;
+				
+				do
+				{
+					do
+					{
+						inFile >> nextWordOnALine;
+						loopCount++;
+					
+						// If in the next 11 words the nextWordOnALine is not the address we expect to encouter break the loop
+						if( loopCount == 11 && (convertHexIntToDecimalString( convertHexStringToUnsignedInt( nextWordOnALine ) )
+							!= convertHexIntToDecimalString( nextSearchingAddress ) ) )
+						{
+							stopSearch = true;
+							break;
+						}
 
-		//		} while ();
-		//	}
-		//	else
-		//		// search from the address of 40000C20 to 4000101C 
-		//}
+					} while( convertHexIntToDecimalString( convertHexStringToUnsignedInt( nextWordOnALine ) )
+						!= convertHexIntToDecimalString( nextSearchingAddress ) );
+
+					if ( stopSearch == true )
+						break;
+
+					inFile >> nextWordOnALine;
+
+					// Store data in a  wordsData queue file structure
+					wordsData.push( nextWordOnALine );
+
+					// Next Address increases by 4
+					nextSearchingAddress += 4;
+
+					// Reset loopCount to 0
+					loopCount = 0;
+
+					// Discard the next two words
+					inFile >> nextWordOnALine;
+					inFile >> nextWordOnALine;
+
+					//if( convertHexIntToDecimalString( 0x40000818 ) == convertHexIntToDecimalString( convertHexStringToUnsignedInt( "40000818" ) ) )
+					//	cout << "Same" << endl;
+
+				} while( nextSearchingAddress <= stopSearchingAddress );
+			}
+			//else
+				// search from the address of 40000C20 to 4000101C 
+		}
+
+
+		cout << "Data in this commands are:" << endl;
+		while ( !wordsData.empty() )
+		{
+			cout << wordsData.front().substr(0,4) << " " << wordsData.front().substr(4,8) << endl;
+			wordsData.pop();
+		}
 
 		cout << endl;
 	} while( getBool( "Keep looking?" ) && !inFile.eof( ) );
@@ -110,7 +164,7 @@ void outputToScreen( const string &address, const string &data, const int &size,
 	cout << "Cycle: " << cycle << endl;
 }
 
-unsigned int convertHexaToUnsignedInt( const string &hexVariable )
+unsigned int convertHexStringToUnsignedInt( const string &hexVariable )
 {
 	// return stoi(hexVariable, nullptr, 16);
 	// Alternative to stoi
@@ -136,6 +190,15 @@ int convertSizeTypeToInt( const string &sizeType )
 	ss >> x;
 
 	return x;
+}
+
+string convertHexIntToDecimalString( const int &hexValue )
+{
+	int x;
+	stringstream ss;
+	ss << dec << hexValue;
+	
+	return ss.str();
 }
 
 // assumes that keyboard input buffer is empty
